1f66c908d43bcc37e052fcee696d9c01a4df0c4a
Andrew <andrew@merison.io>
2019-08-20 19:59:56 -0700

    Broke larger function out into smaller functions
    Separated movement logic from the input handler logic.
    
    Created new global for the file to check sprinting on each physics frame, instead of checking it in multiple  places.

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -3,19 +3,21 @@ extends KinematicBody2D
 # Initial Declarations
 export (int) var speed = 200
 var velocity = Vector2()
+var sprinting = false
 onready var anim = $AnimationPlayer
 onready var sprite = $Sprite
 
 # Main Physics Loop
 func _physics_process(delta):
 	_get_input()
+	_movement_hanlder()
 	_sprite_handler()
 	velocity = move_and_slide(velocity)
 
 # Helper functions
 func _get_input():
 	velocity = Vector2()
-	var sprinting = _is_sprinting()
+	sprinting = _is_sprinting()
 	
 	if Input.is_action_pressed("up"):
 		velocity.y -= 1
@@ -25,17 +27,14 @@ func _get_input():
 		velocity.x -= 1
 	if Input.is_action_pressed("right"):
 		velocity.x += 1
-	
+
+func _movement_hanlder():
 	if sprinting:
 		velocity = velocity.normalized() * speed * 2
 	else:
 		velocity = velocity.normalized() * speed
 
-func _is_sprinting():
-	return Input.is_action_pressed("sprint")
-
 func _sprite_handler():
-	var sprinting = _is_sprinting()
 	var moving = velocity != Vector2.ZERO
 	if sprinting && moving:
 		anim.play("run")
@@ -48,3 +47,6 @@ func _sprite_handler():
 		sprite.flip_h = false
 	if Input.is_action_pressed("left"):
 		sprite.flip_h = true
+
+func _is_sprinting():
+	return Input.is_action_pressed("sprint")
